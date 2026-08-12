154f265dfd8a99a42b7fe4f989faa3fe08b3760c
Fish-Stinky <nggynxern07@gmail.com>
2026-08-12 18:29:33 +0800

    Final Maybe 1.0

--- ET1025_IEP_Group-2.ino
+++ ET1025_IEP_Group-2.ino
@@ -1,33 +1,45 @@
 #include "RichShieldTM1637.h" 
 #include "RichShieldLightSensor.h"
+#include "RichShieldNTC.h"
 #include "RichShieldDHT.h"
 #include "RichShieldIRremote.h"
 #include "RichShieldPassiveBuzzer.h"
 #include <Wire.h>
 
-#define LDR_PIN A2
 #define KNOB_PIN A0
+#define LDR_PIN A2
+#define NTC_PIN A1
 #define RECV_PIN 2
+#define BUZZER_PIN 3
 #define LED_RED 4
 #define LED_GREEN 5
 #define LED_BLUE 6
 #define LED_YELLOW 7
-
-#define DHT_TYPE   DHT11  
+#define DHT_TYPE DHT11  
 #define BUTTON_K1 8
 #define BUTTON_K2 9
 #define CLK 10
 #define DIO 11
-#define DHT_PIN    12
+#define DHT_PIN 12
 TM1637 disp(CLK, DIO);
 IRrecv IR(RECV_PIN);
 DHT dht(DHT_PIN, DHT_TYPE);
-
-//Thershold of sensors
+NTC temper(NTC_PIN);  
+#define BUTTON_CODE 0x45 
+PassiveBuzzer buz(BUZZER_PIN);     //(Power button on Remote)
+//    Thershold of sensors    // 
 int HumThreshold = 65;            //Threshold Huminity (Default value is 65)
 int TempThreshold = 23;           // Threshold Temperture (Defualt value is 23C)
-int LdrSens = 5;                  //Scale from 1 to 10 (Defualt 5)
-int ldrThreshold = map(LdrSens, 1, 10, 50, 950); //Actualy value of Ldr adjusted Ranging from 50 to 950
+int LdrSens = 4;                  //Scale from 1 to 10 (Defualt 4)
+int ldrThreshold = 350;
+int state = 1;
+int previousMenu = -1;  // ADDED: tracks last menu, -1 forces a clear on first run
+bool K1WasPressed = false;
+bool K2WasPressed = false;
+unsigned long previousBlinkTime = 0;  //Compare with previous cloak
+bool ledBlinkState = false;           //iintial state
+const long blinkInterval = 100;       //100ms interval for LED blink
+
 void setup() 
 {
   // put your setup code here, to run once:
@@ -40,31 +52,41 @@ void setup()
   pinMode(LED_GREEN, OUTPUT);
   pinMode(LED_BLUE, OUTPUT);
   pinMode(LED_YELLOW, OUTPUT);
- 
-
+  
 }
 //            MAIN LOOP             //
 void loop() 
 {
-  int Menu = CheckNob();
+  int Menu = CheckNob();    //Check nob Value
+  Serial.println(state);
+  if (Menu != previousMenu) //only clear LEDs on menu change
+  {   
+    clearLEDs();
+    previousMenu = Menu;
+  }
 
   if (Menu == 0)
-  {
-    clearLEDs();   
+  {   
     //    Main logic inside (Mokuns protion)    // 
+    if(state == 1)
+    {
+    digitalWrite(LED_RED, HIGH);
+    smartLight();
+    tempDetection();
+    smartFanControl();
+  }
+  OnOffSwitch();
 
   }
   //Smart light
   if(Menu == 1)
   {
-    clearLEDs();
     BlinkLEDs(LED_YELLOW);
     AdjLdr();
   }
   //Temp Detector
   if(Menu == 2)
   {
-    clearLEDs();
     BlinkLEDs(LED_BLUE);
     AdjTemp();
 
@@ -72,21 +94,18 @@ void loop()
   //Smart FAN
   if(Menu == 3)
   {
-    clearLEDs();
     BlinkLEDs(LED_GREEN);
     AdjHum();
   }
   if(Menu == 4)
   {
-    clearLEDs();
     BlinkLEDs(LED_RED);
-
+    CheckPower();
   }
 
 }
-//            Functions            //
-int CheckNob()
-{
+//            Functions            // (GynXern)
+int CheckNob() {
   int knobVal;
   knobVal = analogRead(KNOB_PIN);
 
@@ -95,8 +114,7 @@ int CheckNob()
   return (knobVal/205);
 }
 
-void clearLEDs()
-{
+void clearLEDs() {
       digitalWrite(LED_RED, LOW);
       digitalWrite(LED_GREEN, LOW);
       digitalWrite(LED_BLUE, LOW);
@@ -104,31 +122,34 @@ void clearLEDs()
       return;
 }
 
-void BlinkLEDs(int LED)
-{
-  digitalWrite(LED, HIGH);
-  delay(250);
-  digitalWrite(LED, LOW);
-  delay(250);
-  return;
+void BlinkLEDs(int LED) {
+  unsigned long currentTime = millis();
+  if (currentTime - previousBlinkTime >= blinkInterval) 
+  {
+    previousBlinkTime = currentTime;
+    ledBlinkState = !ledBlinkState;
+    digitalWrite(LED, ledBlinkState);
+  }
 }
 
-void AdjTemp()
-{
+void AdjTemp() {
 
-  if(digitalRead(BUTTON_K1) == 0)
-  {
+  bool K1Now = (digitalRead(BUTTON_K1) == 0);
+  bool K2Now = (digitalRead(BUTTON_K2) == 0);
+
+  if (K1Now && !K1WasPressed) 
+  {          // only triggers on the moment it's first pressed
     TempThreshold += 1;
-    if(TempThreshold > 28) TempThreshold = 28;
-  while (digitalRead(BUTTON_K1) == 0);  
+    if (TempThreshold > 29) TempThreshold = 29;
   }
-
-  else if(digitalRead(BUTTON_K2) == 0)
+  if (K2Now && !K2WasPressed) 
   {
     TempThreshold -= 1;
-    if(TempThreshold < 18) TempThreshold = 18;
-  while (digitalRead(BUTTON_K2) == 0);  
+    if (TempThreshold < 18) TempThreshold = 18;
   }
+
+  K1WasPressed = K1Now;                  // remember state for next loop pass
+  K2WasPressed = K2Now;
 
   int8_t temp[4];
   int8_t temperature = TempThreshold;
@@ -138,25 +159,27 @@ void AdjTemp()
 	temp[2] = temperature % 10;
 	temp[3] = 12;	          //index of 'C' for celsius degree symbol.
 	disp.display(temp);
-  
+  Serial.print("Temp: "); Serial.println(TempThreshold);
   return;
 }
 
-void AdjHum()
-{
-  if(digitalRead(BUTTON_K1) == 0)
-  {
-    HumThreshold += 5;
-    if(HumThreshold < 90) HumThreshold = 90;
-  while (digitalRead(BUTTON_K1) == 0);
-  }
+void AdjHum() {
+  bool K1Now = (digitalRead(BUTTON_K1) == 0);
+  bool K2Now = (digitalRead(BUTTON_K2) == 0);
 
-  else if(digitalRead(BUTTON_K2) == 0)
+  if (K1Now && !K1WasPressed) 
+  {          // only triggers on the moment it's first pressed
+    HumThreshold += 5;
+    if(HumThreshold > 90) HumThreshold = 90;
+  }
+  if (K2Now && !K2WasPressed) 
   {
     HumThreshold -= 5;
-    if(HumThreshold < 40) HumThreshold = 40;
-  while (digitalRead(BUTTON_K2) == 0);  
+    if (HumThreshold < 40) HumThreshold = 40;
   }
+
+  K1WasPressed = K1Now;                  // remember state for next loop pass
+  K2WasPressed = K2Now;
 
   int8_t temp[4];
   int8_t humi = HumThreshold;
@@ -170,22 +193,107 @@ void AdjHum()
 
 void AdjLdr() 
 {
-  if (digitalRead(BUTTON_K1) == 0) 
-  {
+  bool K1Now = (digitalRead(BUTTON_K1) == 0);
+  bool K2Now = (digitalRead(BUTTON_K2) == 0);
+
+  if (K1Now && !K1WasPressed) 
+  {          // only triggers on the moment it's first pressed
     LdrSens++;
     if (LdrSens > 10) LdrSens = 10;   // cap at 10
-    ldrThreshold = map(LdrSens, 1, 10, 50, 950);  // convert 1-10 to actual threshold
-    Serial.print("LdrSens: "); Serial.println(LdrSens);
-    while (digitalRead(BUTTON_K1) == 0);       // wait for release
+    ldrThreshold = map(LdrSens, 1, 10, 50, 950); 
   }
-
-  if (digitalRead(BUTTON_K2) == 0)
+  if (K2Now && !K2WasPressed) 
   {
     LdrSens--;
     if (LdrSens < 1) LdrSens = 1;     // floor at 1
     ldrThreshold = map(LdrSens, 1, 10, 50, 950);
-    Serial.print("LdrSens: "); Serial.println(LdrSens);
-    while (digitalRead(BUTTON_K2) == 0);
+  }
+  disp.display(LdrSens);
+}
+
+void CheckPower()
+{
+  if(state == 1) 
+  {
+  digitalWrite(LED_GREEN, HIGH);
+  }
+  else
+  {
+  digitalWrite(LED_YELLOW, HIGH);
+  }
+  Serial.print("State: "); Serial.println(state);
+}
+//            Function              // (Mokun)
+void smartLight() {
+  Serial.print("Light: "); Serial.println(analogRead(LDR_PIN));Serial.print("  // LdrTHRE: "); Serial.println(ldrThreshold);
+  if (analogRead(LDR_PIN) < ldrThreshold) 
+  {
+    digitalWrite(LED_YELLOW, HIGH);
+  } 
+  else 
+  {
+    digitalWrite(LED_YELLOW, LOW);
   }
 }
 
+void tempDetection() {
+  int8_t temperature;
+  temperature = temper.getTemperature();
+  Serial.print("Temp: "); Serial.println(temperature);Serial.print(" // TempTHRE: "); Serial.println(TempThreshold);
+  if (TempThreshold <= temperature)
+  {
+    digitalWrite(LED_BLUE, HIGH);    
+  } 
+  else 
+  {
+    digitalWrite(LED_BLUE, LOW);           
+  }
+
+  int8_t temp[4];
+ 
+  temp[0] = INDEX_BLANK;
+  temperature %= 100;
+	temp[1] = temperature / 10;
+	temp[2] = temperature % 10;
+	temp[3] = 12;	          //index of 'C' for celsius degree symbol.
+	disp.display(temp);
+}
+
+void smartFanControl() {
+  float humidity = dht.readHumidity();
+  Serial.print("Humi: "); Serial.println(humidity);Serial.print("  // HumTHRE: "); Serial.println(HumThreshold);
+
+  if (humidity > HumThreshold) 
+  {
+    digitalWrite(LED_GREEN, HIGH);
+  } 
+  else 
+  {
+    digitalWrite(LED_GREEN, LOW);
+  }
+}
+
+void OnOffSwitch() 
+{
+  if (IR.decode())
+  {
+    if(IR.isReleased())
+    {
+    if (IR.keycode == BUTTON_CODE) 
+      {
+        state = (state == 1) ? 0 : 1;   // clean toggle between 0 and 1
+
+        if (state == 1) {
+          buz.playTone(2000, 800);      // higher pitch = turning ON
+          digitalWrite(LED_RED, HIGH);
+        } 
+        else 
+        {
+          buz.playTone(500, 800);       // lower pitch = turning OFF
+          digitalWrite(LED_RED, LOW);
+        }
+      }
+    }
+    IR.resume();
+  }
+}
